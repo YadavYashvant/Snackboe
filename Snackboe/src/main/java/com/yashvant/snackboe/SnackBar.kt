5ff6d8137d8ef2ada15bbfd5690bc4fb66350b62
YadavYashvant <yashvantyadav855@gmail.com>
2024-04-26 16:34:45 +0530

    Initial commit

---
+++ Snackboe/src/main/java/com/yashvant/snackboe/SnackBar.kt
@@ -0,0 +1,13 @@
+package com.yashvant.snackboe
+
+import android.content.Context
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class SnackBoe {
+
+    fun SnackBoer(context: Context, message: String, mode: String = "success"){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
