fb3cf582595880c4599c1b71e98cec21ffc94dd7
YadavYashvant <yashvantyadav855@gmail.com>
2024-04-29 23:46:43 +0530

    Writing the UI for snackbar

--- Snackboe/src/main/java/com/yashvant/snackboe/SnackBar.kt
+++ Snackboe/src/main/java/com/yashvant/snackboe/SnackBar.kt
@@ -2,6 +2,16 @@ package com.yashvant.snackboe
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.google.android.material.snackbar.Snackbar
 
 object SnackbarHelper {
